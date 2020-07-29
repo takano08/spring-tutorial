5e9b79ea831005d5cea660b307447209d36723ef
takano <ri1saxxminny83xx@gmail.com>
2020-07-29 17:27:50 +0900

    rest-service

---
+++ nonrest/src/main/java/com/example/nonrest/NonrestApplication.java
@@ -0,0 +1,13 @@
+//package com.example.nonrest;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class NonrestApplication {
+
+	//public static void main(String[] args) {
+		//SpringApplication.run(NonrestApplication.class, args);
+	//}
+
+//}
